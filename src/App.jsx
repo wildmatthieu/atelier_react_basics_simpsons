8fd02a10793d3906e19601304ed0bbaa6490bd37
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-03-27 23:22:07 +0100

    setup for live coding on useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
+// import Confetti from "react-confetti";
+// import useWindowSize from "react-use/lib/useWindowSize";
 import "./App.css";
 import Character from "./components/Character";
 
 function App() {
+  // const { width, height } = useWindowSize();
+
   const homer = {
-    firstname: "homer",
-    lastname: "simpson",
+    firstname: "Homer",
+    lastname: "Simpson",
     imagePath:
-      "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+      "https://www.stickees.com/files/cartoon/the-simpsons/2247-homer-simpson-beer.png",
+    happyHomer:
+      "https://www.stickees.com/files/cartoon/the-simpsons/2248-homer-simpson-happy.png",
   };
 
-  const lisa = {
-    firstname: "lisa",
-    lastname: "bla",
+  const bart = {
+    firstname: "Bart",
+    lastname: "Simpson",
     imagePath:
-      "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+      "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
   };
 
   return (
-    <>
+    <div className="app-container">
       <Character
         firstname={homer.firstname}
         lastname={homer.lastname}
         imagePath={homer.imagePath}
+        happyHomer={homer.happyHomer}
       />
       <Character
-        firstname={lisa.firstname}
-        lastname={lisa.lastname}
-        imagePath={lisa.imagePath}
+        firstname={bart.firstname}
+        lastname={bart.lastname}
+        imagePath={bart.imagePath}
       />
-    </>
+      <img
+        src="https://www.stickees.com/files/cartoon/the-simpsons/2370-pink-donut.png"
+        alt="donut"
+        className="donut-infinite"
+      />
+    </div>
   );
 }
 
 export default App;
+
+{
+  /* {homerDonuts >= 20 && (
+  <Confetti
+    width={width}
+    height={height}
+    numberOfPieces={2000}
+    recycle={false}
+  />
+)} */
+}
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,20 +1,34 @@
-import PropTypes from 'prop-types';
+import "./Character.css";
+import PropTypes from "prop-types";
 
-function Character({firstname, lastname, imagePath})
-{
-
-    return (
-        <div>
-            <h2>{firstname} {lastname}</h2>
-            <img src={imagePath} alt={firstname} />
-        </div>
-    )
+function Character({ firstname, lastname, imagePath }) {
+  return (
+    <div className="character-section">
+      <figure className="character-card-container">
+        <h2>
+          {firstname} {lastname} 0 🍩
+        </h2>
+        <img className="img-box" src={imagePath} alt={firstname} />
+      </figure>
+      <button className="button-5" type="button">
+        feed me
+        <img
+          src="https://www.stickees.com/files/cartoon/the-simpsons/2370-pink-donut.png"
+          alt="donut"
+          className="donut-btn"
+        />
+      </button>
+    </div>
+  );
 }
 
-Character.propTypes= {
-    lastname: PropTypes.string.isRequired,
-    firstname: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired
-}
+Character.propTypes = {
+  lastname: PropTypes.string.isRequired,
+  firstname: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  donuts: PropTypes.number.isRequired,
+  setDonuts: PropTypes.number.isRequired,
+  happyHomer: PropTypes.string,
+};
 
 export default Character;
