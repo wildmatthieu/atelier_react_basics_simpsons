c6b3b38a361f52e7042f952271b4ba31c47adb48
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-03-27 17:32:52 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import './App.css'
-import Character from './components/Character';
+import "./App.css";
+import Character from "./components/Character";
 
 function App() {
-
-    
-
   const homer = {
     firstname: "homer",
     lastname: "simpson",
-    imagePath: "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"
+    imagePath:
+      "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
   };
 
   const lisa = {
     firstname: "lisa",
-    lastname: "blabla",
-    imagePath: "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"
+    lastname: "bla",
+    imagePath:
+      "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
   };
 
   return (
     <>
-      <Character firstname={homer.firstname} lastname={homer.lastname} imagePath={homer.imagePath}/>
-      <Character firstname={lisa.firstname} lastname={lisa.lastname} imagePath={lisa.imagePath}/>
+      <Character
+        firstname={homer.firstname}
+        lastname={homer.lastname}
+        imagePath={homer.imagePath}
+      />
+      <Character
+        firstname={lisa.firstname}
+        lastname={lisa.lastname}
+        imagePath={lisa.imagePath}
+      />
     </>
-  )
+  );
 }
- 
-export default App
+
+export default App;
